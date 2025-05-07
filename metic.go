55c64d290270a6089beb8d4fb89d55c5ee538094
kayode oyedeji <koyedeji.ng@gmail.com>
2025-05-07 23:01:05 +0100

    add comment

--- metic.go
+++ metic.go
@@ -1,5 +1,10 @@
 package metic
 
+// Add it accept two parameters of type int and perform addition operation.
+//
+// Read more about the match operation here: [mathsisfun]
+//
+// [mathsisfun]: https://mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
